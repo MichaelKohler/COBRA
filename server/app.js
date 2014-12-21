c43a7a35b8b8249252669018332004183ba1dfea
Michael Kohler <mkohler@picobudget.com>
2014-12-21 19:09:20 +0100

    fix CORS

--- server/app.js
+++ server/app.js
@@ -28,9 +28,11 @@
   }
 
   var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept"');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Max-Age', 1728000);
     next();
   }
 
